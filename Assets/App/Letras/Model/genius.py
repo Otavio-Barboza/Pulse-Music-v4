5c3be85fdc64ec779b7fcec92cc93f362dac62e9
Otavio-Barboza <162923959+Otavio-Barboza@users.noreply.github.com>
2026-06-17 09:54:48 -0300

    Correção da API da Genius

--- Assets/App/Letras/Model/genius.py
+++ Assets/App/Letras/Model/genius.py
@@ -11,18 +11,31 @@ def ler_env():
 
 class Genius(lyricsgenius.Genius):
     def __init__(
-        self, 
-        access_token = ler_env(), 
-        response_format = "plain", 
-        timeout = 15, 
-        sleep_time = 0.2, 
-        verbose = True, 
-        remove_section_headers = True, 
-        skip_non_songs = True, 
-        excluded_terms = ["(Remix)", "(Live)"], 
-        replace_default_terms = False, 
-        retries = 0, 
-        user_agent = "", 
-        proxy = None
+        self,
+        access_token=ler_env(),
+        response_format="plain",
+        timeout=15,
+        sleep_time=0.2,
+        remove_section_headers=True,
+        skip_non_songs=True,
+        excluded_terms=["(Remix)", "(Live)"],
+        replace_default_terms=False,
+        retries=0,
+        user_agent="",
+        proxy=None,
+        per_page=5
     ):
-        super().__init__(access_token, response_format, timeout, sleep_time, verbose, remove_section_headers, skip_non_songs, excluded_terms, replace_default_terms, retries, user_agent, proxy)
+        super().__init__(
+            access_token=access_token,
+            response_format=response_format,
+            timeout=timeout,
+            sleep_time=sleep_time,
+            remove_section_headers=remove_section_headers,
+            skip_non_songs=skip_non_songs,
+            excluded_terms=excluded_terms,
+            replace_default_terms=replace_default_terms,
+            retries=retries,
+            user_agent=user_agent,
+            proxy=proxy,
+            per_page=per_page
+        )
